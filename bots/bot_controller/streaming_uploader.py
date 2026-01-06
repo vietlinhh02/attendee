ba98fea2b3f9741d602c62fb4634b05f9b265518
Viet Linh <nvlinh0607@gmail.com>
2026-01-06 16:18:12 +0700

    fix(infra): configure MinIO and AWS endpoints

--- bots/bot_controller/streaming_uploader.py
+++ bots/bot_controller/streaming_uploader.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 class StreamingUploader:
     def __init__(self, bucket, key, chunk_size=5242880):  # 5MB chunks
-        self.s3_client = boto3.client("s3", endpoint_url=os.getenv("AWS_ENDPOINT_URL"))
+        self.s3_client = boto3.client("s3", endpoint_url=os.getenv("Minio_URL"))
         self.bucket = bucket
         self.key = key
         self.chunk_size = chunk_size
